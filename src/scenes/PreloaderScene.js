0f770f524110dbf6c2735ec75220058b8be63f26
Ewan Brinkman <ewanbrinkman@gmail.com>
2022-10-15 16:00:28 -0700

    Fix door animation load path

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -11,7 +11,7 @@ export default class PreloaderScene extends Phaser.Scene {
         // this.load.spritesheet('playerGreen', 'assets/playerGreen.png', 24, 24, 24);
         // this.load.spritesheet('playerRed', 'assets/playerRed.png', {frameWidth: 24, frameHeight: 24});
         // this.load.spritesheet('playerYellow', 'assets/playerYellow.png', {frameWidth: 24, frameHeight: 24});
-        this.load.spritesheet('door', 'bamboo-doorv3.png',{ frameWidth: 68, frameHeight: 100 });
+        this.load.spritesheet('door', 'assets/bamboo-doorv3.png',{ frameWidth: 68, frameHeight: 100 });
     }
 
     create() {
@@ -29,7 +29,6 @@ export default class PreloaderScene extends Phaser.Scene {
             repeat: -1
         });
 
-        this.scene.start(SCENE_KEYS.title);
         this.anims.create({
             key: 'closed',
             frames: [ { key: 'door', frame: 0 } ],
@@ -42,16 +41,20 @@ export default class PreloaderScene extends Phaser.Scene {
             frameRate: 10,
             repeat: -1
         });
+
         this.anims.create({
             key: 'open',
             frames: [ { key: 'door', frame: 6 } ],
             frameRate: 20
         });
+
         this.anims.create({
             key: 'closing',
             frames: this.anims.generateFrameNumbers('door', { start: 6, end: 0 }),
             frameRate: 10,
             repeat: -1
         });
+
+        this.scene.start(SCENE_KEYS.title);
     }
 };
