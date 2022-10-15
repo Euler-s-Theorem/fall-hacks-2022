a50c34c2bbd7be6c3ed4418620f8f502b2a7a641
Ewan Brinkman <ewanbrinkman@gmail.com>
2022-10-15 16:02:47 -0700

    Remove timer text shadow

--- src/scenes/HUDScene.js
+++ src/scenes/HUDScene.js
@@ -12,11 +12,8 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         this.text = this.add.text(800, 20, "Time left: ", { fontSize: '40px', fill: 'white', fontWeight: 'bold' });
-        this.text.setShadow(10, 0, 'rgba(255, 2, 100, 1)', 0);
+        // this.text.setShadow(10, 0, 'rgba(255, 2, 100, 1)', 0);
     }
-
-
-
 
     update() {
 
